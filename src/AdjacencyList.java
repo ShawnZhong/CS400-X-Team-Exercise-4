07b7abcb8d9d3244886242d1b1c63c77d0977d19
Shawn Zhong <public@shawnzhong.com>
2018-04-10 14:55:03 -0500

    Minor

--- src/AdjacencyList.java
+++ src/AdjacencyList.java
@@ -1,93 +1,96 @@
 import java.util.ArrayList;
 
 public class AdjacencyList<T> {
-	private ArrayList<LinkedList<T>> list; // holds the list to edges
-	
-	AdjacencyList(){
-		list = new ArrayList<LinkedList<T>>();
-	}
-	
-	/**
-	 * Add given vertex into the adjacency list
-	 * Written for directional graphs - if graph is non-directional, method must be called twice
-	 * @param vertex - vertex to be added
-	 */
-	public void addVertex(T vertex) {
-		list.add(new LinkedList<T>()); // duplicate exception will be thrown if duplicate is entered
-	}
-	
-	/**
-	 * Removes the given vertex from adjacency list
-	 * @param vertex - vertex to be removed
-	 * @param index - index that vertex's edges are located
-	 */
-	public void removeVetex(T vertex, int index) {
-		list.remove(index);
-		// removes every edge to vertex within list
-		for(int x = 0; x < list.size(); x++) {
-			// should have no duplicates within the LinkedList
-			try {
-				list.get(x).remove(vertex); 
-			}
-			catch (RuntimeException e){
-				
-			}
-		}
-		
-	}
-	
-	/**
-	 * Adds edge between source and destination
-	 * Written for directional graphs - if graph is non-directional, method must be called twice
-	 * @param source - beginning of edge
-	 * @param destination - end of edge
-	 * @param index - index of source's edges are located
-	 */
-	public void addEdge(T source, T destination, int index) {
-		// adds destination to list
-		list.get(index).add(destination);
-	}
-	
-	/**
-	 * returns the edges of a specified vertex
-	 * @param index - index of the vertex's whose edges are stored
-	 * 
-	 * @return list of edges from vertex
-	 */
-	public ArrayList<T> getEdges( int index ){		
-		return list.get(index).toArrayList();
-	}
-	
-	/**
-	 * Removes edge between source and destination
-	 * Written for directional graphs, if non-directional, method should be called twice
-	 * @param source - beginning of the edge
-	 * @param destination - end of the edge
-	 * @param int index - index of the source's edges are located
-	 */
-	public void removeEdge(T source, T destination, int index) {
-		// source does not exist in vertices
-		if( index == -1 ) {
-			throw new IllegalArgumentException("Source not found");
-		}
-		// removes edge from given vertices
-		list.get(index).remove(destination);
-	}
-	
-	/**
-	 * Checks if two vertices have an edge between each other
-	 * @param source - beginning of edge
-	 * @param destination - end of edge
-	 * @param index - index of source's edges within list
-	 * 
-	 * @return true if an edge exists between two vertices
-	 */
-	public boolean isConntected(T source, T destination, int index) {
-		T value = list.get(index).search(destination);
-		// value was not found
-		if (value == null) {
-			return false;
-		}
-		return true;
-	}
+    private ArrayList<LinkedList<T>> list; // holds the list to edges
+
+    AdjacencyList() {
+        list = new ArrayList<LinkedList<T>>();
+    }
+
+    /**
+     * Add given vertex into the adjacency list
+     * Written for directional graphs - if graph is non-directional, method must be called twice
+     *
+     * @param vertex - vertex to be added
+     */
+    public void addVertex(T vertex) {
+        list.add(new LinkedList<T>()); // duplicate exception will be thrown if duplicate is entered
+    }
+
+    /**
+     * Removes the given vertex from adjacency list
+     *
+     * @param vertex - vertex to be removed
+     * @param index  - index that vertex's edges are located
+     */
+    public void removeVetex(T vertex, int index) {
+        list.remove(index);
+        // removes every edge to vertex within list
+        for (int x = 0; x < list.size(); x++) {
+            // should have no duplicates within the LinkedList
+            try {
+                list.get(x).remove(vertex);
+            } catch (RuntimeException e) {
+
+            }
+        }
+
+    }
+
+    /**
+     * Adds edge between source and destination
+     * Written for directional graphs - if graph is non-directional, method must be called twice
+     *
+     * @param source      - beginning of edge
+     * @param destination - end of edge
+     * @param index       - index of source's edges are located
+     */
+    public void addEdge(T source, T destination, int index) {
+        // adds destination to list
+        list.get(index).add(destination);
+    }
+
+    /**
+     * returns the edges of a specified vertex
+     *
+     * @param index - index of the vertex's whose edges are stored
+     * @return list of edges from vertex
+     */
+    public ArrayList<T> getEdges(int index) {
+        return list.get(index).toArrayList();
+    }
+
+    /**
+     * Removes edge between source and destination
+     * Written for directional graphs, if non-directional, method should be called twice
+     *
+     * @param source      - beginning of the edge
+     * @param destination - end of the edge
+     * @param int         index - index of the source's edges are located
+     */
+    public void removeEdge(T source, T destination, int index) {
+        // source does not exist in vertices
+        if (index == -1) {
+            throw new IllegalArgumentException("Source not found");
+        }
+        // removes edge from given vertices
+        list.get(index).remove(destination);
+    }
+
+    /**
+     * Checks if two vertices have an edge between each other
+     *
+     * @param source      - beginning of edge
+     * @param destination - end of edge
+     * @param index       - index of source's edges within list
+     * @return true if an edge exists between two vertices
+     */
+    public boolean isConntected(T source, T destination, int index) {
+        T value = list.get(index).search(destination);
+        // value was not found
+        if (value == null) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -5,144 +5,145 @@ import java.util.ArrayList;
  * limited functions of a Linked list
  * does not allow duplicates
  */
-public class LinkedList<T>{
-	/**
-	 * Inner generic Class for nodes of the linked list
-	 */
-	private class LinkedNode<A> {
-		/**
-		 * Instance variables and constructors
-		 */
-		private A data;
-		private LinkedNode<A> next;
+public class LinkedList<T> {
+    /**
+     * Instance variables and constructors
+     */
+    private LinkedNode<T> root;
 
-		LinkedNode(A data) {
-			this.data = data;
-			next = null;
-		}
+    LinkedList() {
+        root = null;
+    }
 
-		LinkedNode(A data, LinkedNode<A> next) {
-			this.data = data;
-			this.next = next;
-		}
-	}
+    /**
+     * Checks if link is empty
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
 
-	/**
-	 * Instance variables and constructors
-	 */
-	private LinkedNode<T> root;
+    /**
+     * Search for item within the list
+     *
+     * @param key - data that the list is searching for
+     * @return - returns the data if found
+     */
+    public T search(T key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data.equals(key)) {
+            return root.data;
+        } else {
+            LinkedNode<T> list = root;
+            while (list.next != null) {
+                if (list.next.data.equals(key)) {
+                    return list.data;
+                }
+                list = list.next;
+            }
+        }
+        return null;
+    }
 
-	LinkedList() {
-		root = null;
-	}
+    /**
+     * Add value to the linked list according to CS400 convention
+     *
+     * @param T item - item to be inserted
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public void add(T item) throws DuplicateException {
+        if (item == null) {
+            throw new IllegalArgumentException("item is null");
+        }
+        if (root == null) {
+            root = new LinkedNode<T>(item);
+        } else if (root.data.equals(item)) {
+            throw new DuplicateException();
+        } else {
+            LinkedNode<T> list = root;
+            while (list.next != null) {
+                // if item is within the list
+                if (item == list.next.data) {
+                    throw new DuplicateException();
+                } else {
+                    list = list.next;
+                }
+            }
+            // inserts item at the very end of the list
+            list.next = new LinkedNode(item, list.next);
+        }
+    }
 
-	/**
-	 * Checks if link is empty
-	 */
-	public boolean isEmpty() {
-		return root == null;
-	}
-	
-	/**
-	 * Search for item within the list
-	 * @param key - data that the list is searching for
-	 * @return - returns the data if found
-	 */
-	public T search( T key) {
-		if( root == null ) {
-			return null;
-		}
-		if(root.data.equals(key)) {
-			return root.data;
-		}
-		else {
-			LinkedNode<T> list = root;
-			while(list.next != null) {
-				if(list.next.data.equals(key)){
-					return list.data;
-				}
-				list = list.next;
-			}
-		}
-		return null;
-	}
+    /**
+     * Removes the given value from the linked list
+     *
+     * @param item - value to be removed
+     */
+    public void remove(T item) {
+        if (item == null) {
+            throw new IllegalArgumentException("item is null");
+        }
+        // list is empty
+        if (root == null)
+            throw new NullPointerException();
+        // item == root
+        if (root.data.equals(item)) {
+            root = root.next;
+            return;
+        }
+        // stores current and previous nodes
+        LinkedNode<T> cur = root;
+        LinkedNode<T> prev = null;
 
-	/**
-	 * Add value to the linked list according to CS400 convention
-	 * @param T item - item to be inserted
-	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void add(T item) throws DuplicateException {
-		if(item == null) {
-			throw new IllegalArgumentException("item is null");
-		}
-		if (root == null) {
-			root = new LinkedNode<T>(item);
-		}
-		else if(root.data.equals(item)) {
-			throw new DuplicateException();
-		}
-		else {
-			LinkedNode<T> list = root;
-			while (list.next != null) {
-				// if item is within the list
-				if (item == list.next.data) {
-					throw new DuplicateException();
-				} else {
-					list = list.next;
-				}
-			}
-			// inserts item at the very end of the list
-			list.next = new LinkedNode(item, list.next);
-		}
-	}
+        // iterates through the list to find item to be removed
+        while (cur != null && !cur.data.equals(item)) {
+            prev = cur;
+            cur = cur.next;
+        }
 
-	/**
-	 * Removes the given value from the linked list
-	 * @param item - value to be removed
-	 */
-	public void remove(T item) {
-		if( item == null) {
-			throw new IllegalArgumentException("item is null");
-		}
-		// list is empty
-		if (root == null)
-			throw new NullPointerException();
-		// item == root
-		if (root.data.equals(item)) {
-			root = root.next;
-			return;
-		}
-		// stores current and previous nodes
-		LinkedNode<T> cur = root;
-		LinkedNode<T> prev = null;
-		
-		// iterates through the list to find item to be removed
-		while (cur != null && !cur.data.equals(item)) {
-			prev = cur;
-			cur = cur.next;
-		}
-		
-		// item was not within the list
-		if (cur == null)
-			throw new IndexOutOfBoundsException("item is not within the list");
+        // item was not within the list
+        if (cur == null)
+            throw new IndexOutOfBoundsException("item is not within the list");
 
-		// delete cur node
-		prev.next = cur.next;
-	}
-	
-	/**
-	 * Converts linked list into an array list
-	 * @return - array list with linked list data
-	 */
-	public ArrayList<T> toArrayList(){
-		LinkedNode<T> list = root;
-		ArrayList<T> toReturn = new ArrayList<T>();
-		while(list != null) {
-			toReturn.add(list.data);
-			list = list.next;
-		}
-		return toReturn;
-	}
+        // delete cur node
+        prev.next = cur.next;
+    }
+
+    /**
+     * Converts linked list into an array list
+     *
+     * @return - array list with linked list data
+     */
+    public ArrayList<T> toArrayList() {
+        LinkedNode<T> list = root;
+        ArrayList<T> toReturn = new ArrayList<T>();
+        while (list != null) {
+            toReturn.add(list.data);
+            list = list.next;
+        }
+        return toReturn;
+    }
+
+    /**
+     * Inner generic Class for nodes of the linked list
+     */
+    private class LinkedNode<A> {
+        /**
+         * Instance variables and constructors
+         */
+        private A data;
+        private LinkedNode<A> next;
+
+        LinkedNode(A data) {
+            this.data = data;
+            next = null;
+        }
+
+        LinkedNode(A data, LinkedNode<A> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 
 }
